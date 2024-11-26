c6ca996ea06eb881bd89bfdeb48179fc2eef294e
unknown <andy.luarte@gmail.com>
2024-11-26 01:10:58 -0300

    10 commit

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from .views import home
 
 urlpatterns = [
     path('', home, name='home'),  # Ruta para "/"
-    path('home/', home, name='home'),  # Ruta para "/home"
+    path('home', home, name='home'),  # Ruta para "/home"
 ]
